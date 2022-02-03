0d29a83d4e57d09a739dce0ac63f8107dc085575
ndondoRugaba <sagemuho@gmail.com>
2022-02-03 21:19:48 +0200

    added Props of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
+// components
+import QuestionCard from './components/QuestionCard';
 
 
 const App =() => {
+  
+  const startTrivia = async () =>{
+
+  }
+
+  const checkAnswer = (e:React.MouseEvent <HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () =>{
+
+  }
+
   return (
-    <div className="App">Quiz</div>
+    <div className="App">
+     <h1>REACT QUIZ</h1>
+     
+     <button className='start' onClick={startTrivia}>
+         Start
+     </button>
+     <p className="score">Score:</p>
+     <p>Loading Questions ...</p>
+       <QuestionCard question={''} answer={[]} callback={undefined} userAnswer={''} questionNr={0} totalQuestions={0}/>
+
+       <button className='next' onClick={nextQuestion}>
+          Next Question
+     </button>
+
+      </div>
   );
+
 }
 export default App;
 
