835278aba204efbcd88eb8afe72fe4f99fc2186d
ndondoRugaba <sagemuho@gmail.com>
2022-02-07 20:40:30 +0200

    styling buttons

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -42,15 +42,28 @@ h1{
     background-image:linear-gradient(180deg,#fff,#87f1ff);
     backgroud-size:100%;
     background-clip:text;
-    --webkit-background-clip:text;
-    --webkit-text-fill-color:transparent;
-    --moz-background-clip:text;
-    --moz-text-fill-color:transparent;
+    -webkit-background-clip:text;
+    -webkit-text-fill-color:transparent;
+    -moz-background-clip:text;
+    -moz-text-fill-color:transparent;
     filter:drop-shadow(2px 2px #0085a3);
     font-size:70px;
+    font-weight:400;
     text-align:center;
     margin:20px;
-
-
+}
+.start , .next{
+    cursor:pointer;
+    background:linear-gradient(180deg,#fff, #ffcc91);
+    border:2px solid #d38558;
+    box-shadow:0px 5px 10px rgba(0,0,0,0.25);
+    border-radius:10px;
+    height:40px;
+    margin:20px 0;
+    padding: 0 40px;
+}
+.start{
+    max-width:200px;
+    
 }
 `
