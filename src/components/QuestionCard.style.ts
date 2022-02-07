bf7ed5f5bdb3e8c5e5c381087bc9965a0841995f
ndondoRugaba <sagemuho@gmail.com>
2022-02-07 20:51:22 +0200

    styling the components

---
+++ src/components/QuestionCard.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+max-width:1100px;
+background:#ebfeff;
+border-radius:10px;
+border:2px solid #0085a3;
+padding:20px;
+box-shadow:0px 5px 10px rgba(0,0,0,0.25);
+text-align:center:
+
+p{
+    font-size:1rem;
+}
+
+`
